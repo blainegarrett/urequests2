c7bb951055ff3da7a95ceca15950f6b471caba75
Blaine F Garrett <blaine@blainegarrett.com>
2017-06-07 08:52:11 -0500

    Slight cleanup

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,8 +4,6 @@ Test Suite for urlrequests2
 
 import unittest
 import urequests2
-import logging
-import urllib2
 
 
 class BaseCase(unittest.TestCase):
@@ -24,10 +22,12 @@ class BaseCase(unittest.TestCase):
 class StatusCodeTests(BaseCase):
     def test_404(self):
         # Ensure we get the correct Exception on a 404 Not Found
-        self.assertRaises(urequests2.HTTPNotFound, urequests2.get, 'http://google.com/404')
+        self.assertRaises(urequests2.HTTPNotFound, urequests2.get,
+                          'http://google.com/404')
 
     def test_500(self):
-        self.assertRaises(urequests2.HTTPInternalError, urequests2.post, "http://www.mplsart.com/api/posts?spork=true")
+        self.assertRaises(urequests2.HTTPInternalError, urequests2.post,
+                          'http://www.mplsart.com/api/posts?spork=true')
 
     def test_200(self):
         # Base case of testing a 200
@@ -42,8 +42,8 @@ class ConnectionCodeTests(BaseCase):
         # TODO: Mock out actual socket connection so as not to do IO in tests
 
         try:
-            result = urllib2.urlopen('http://notarealdomainxx111.x/api/derp/')
+            result = urequests2.get('http://notarealdomainxx111.x/api/derp/')
             self.fail("Expected test to fail. Instead received result: %s" % result)
-        except Exception, e:
+        except urequests2.URLError, e:
             expected_err = "<urlopen error [Errno 8] nodename nor servname provided, or not known>"
             self.assertEquals(str(e), expected_err)
